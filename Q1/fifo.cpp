22930741b4561d9782ddcc6130dcb9132978a27f
Chris Gibb <chris.gibb@outlook.com>
2017-11-21 10:33:15 -0500

    initial run impl

--- Q1/fifo.cpp
+++ Q1/fifo.cpp
@@ -2,6 +2,8 @@
 
 #include "fifo.hpp"
 
+//adapted from http://meansofmine.blogspot.ca/2011/04/c-program-to-implement-fifo-page.html
+
 ::Comp3473Ass4::FIFO::FIFO(int frameNum) :
 ::Comp3473Ass4::PageRepAlgo<::Comp3473Ass4::FIFO>::PageRepAlgo(frameNum)
 {
@@ -18,7 +20,7 @@ void ::Comp3473Ass4::FIFO::setFrameNumber(int num)
     for(int i = 0; i != num; ++i)
     {
         this->pages.push_back(0);
-        this->frames.push_back(0);
+        this->frames.push_back(-1);
     }
 }
 
@@ -31,5 +33,24 @@ void ::Comp3473Ass4::FIFO::reset()
 
 int ::Comp3473Ass4::FIFO::run()
 {
-
+    int j = 0;
+    int avail = 0;
+    for(int i = 1; i <= this->pages.size(); ++i)
+    {
+        avail = 0;
+        for(int k = 0; k < this->frames.size(); ++k)
+        {
+            if(this->frames[k] == this->pages[i])
+            {
+                avail = 1;
+            }
+            if(avail == 0)
+            {
+                this->frames[j] = this->pages[i];
+                j = (j + 1) % this->frames.size();
+                this->faults++;
+            }
+        }
+    }
+    return this->faults;
 }
--- Q1/main.cpp
+++ Q1/main.cpp
@@ -12,6 +12,9 @@ int main(int argc,char*argv[])
 
     ::Comp3473Ass4::FIFO fifo(5);
 
+    fifo.setReferenceString<std::vector<int>>({5,7,5,6,7});
+
     std::cout<<lru.run()<<std::endl;
+    std::cout<<fifo.run()<<std::endl;
     return 0;
 }
